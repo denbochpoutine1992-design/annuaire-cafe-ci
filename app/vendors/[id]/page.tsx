14ba03ace8efa87b579ce93a7ac62e49dc3ac73b
denbochpoutine1992-design <denbochpoutine1992@gmail.com>
2026-09-21 22:16:19 +0000

    Update page.tsx

--- app/vendors/[id]/page.tsx
+++ app/vendors/[id]/page.tsx
@@ -514,7 +514,7 @@ export default function VendorDetailPage() {
   vendorId={vendor.id}
   vendorName={vendor.name}
   name={p.name}
-  price={Number(p.price)}
+  price={parseInt(String(p.price).replace(/\D/g, ""), 10) || 0}
   image={p.photoUrl}
 />
                       </div>
