c700131f7dc98a6d8bbc0716dca43d88604cb422
denbochpoutine1992-design <denbochpoutine1992@gmail.com>
2026-09-21 21:25:27 +0000

    Create AddToCartButton.tsx

---
+++ components/AddToCartButton.tsx
@@ -0,0 +1,46 @@
+```tsx
+"use client";
+
+import { useCart } from "./CartProvider";
+
+type Props = {
+  productId: string;
+  vendorId: string;
+  vendorName: string;
+  name: string;
+  price: number;
+  image?: string;
+};
+
+export default function AddToCartButton({
+  productId,
+  vendorId,
+  vendorName,
+  name,
+  price,
+  image,
+}: Props) {
+  const { addToCart } = useCart();
+
+  function handleAdd() {
+    addToCart({
+      productId,
+      vendorId,
+      vendorName,
+      name,
+      price,
+      image,
+    });
+  }
+
+  return (
+    <button
+      type="button"
+      onClick={handleAdd}
+      className="w-full rounded-xl bg-[#7C4A2D] px-4 py-3 font-semibold text-white transition hover:bg-[#603820]"
+    >
+      🛒 Ajouter au panier
+    </button>
+  );
+}
+```
